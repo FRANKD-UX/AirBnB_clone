e128205ed3f6ccfd626e81f50a1393c86f00fcd9
FRANKD-UX <frankdafrica@gmail.com>
2024-11-17 12:43:20 -0800

    F

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,51 @@
 #!/usr/bin/env python3
 import json
 import os
+from models.base_model import BaseModel
+from datetime import datetime
 
 
 class FileStorage:
     """
-    Handles storage of objects to a JSON file.
+    Handles saving and loading data from a JSON file.
     """
+
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """
-        Returns the dictionary __objects.
+        Return the dictionary of all objects.
         """
         return FileStorage.__objects
 
     def new(self, obj):
         """
-        Adds a new object to __objects dictionary.
+        Add a new object to the objects dictionary.
         """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
         """
-        Saves the current objects to the JSON file.
+        Save all objects to the file.
         """
-        with open(self.__file_path, 'w') as f:
-            json.dump({
-                key: obj.to_dict()
-                for key, obj in FileStorage.__objects.items()
-            }, f)
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump({key: obj.to_dict()
+                      for key, obj in FileStorage.__objects.items()}, f)
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects if the file exists.
+        Load all objects from the file if it exists.
         """
-        if os.path.exists(self.__file_path):
-            try:
-                with open(self.__file_path, 'r') as f:
-                    objects = json.load(f)
-                    for key, value in objects.items():
-                        cls_name = value['__class__']
-                        cls = all_classes.get(cls_name)
-                        if cls:
-                            FileStorage.__objects[key] = cls(**value)
-            except json.JSONDecodeError:
-                # Reset file content if it's invalid
-                with open(self.__file_path, 'w') as f:
-                    f.write('{}')
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                objs = json.load(f)
+                for key, value in objs.items():
+                    class_name = value["__class__"]
+                    if class_name == "BaseModel":
+                        obj = BaseModel(**value)
+                    # Add other model types here if necessary
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
