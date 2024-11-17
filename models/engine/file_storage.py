4a2ee0918975567870babf62a30418085fa183fa
FRANKD-UX <frankdafrica@gmail.com>
2024-11-17 15:22:51 -0800

    F

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Module for FileStorage class."""
-
+"""FileStorage class for storing and retrieving objects"""
 import json
 from models.base_model import BaseModel
+from models.user import User  # Import User class
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes
-    JSON file to instances.
-    """
+    """Serializes instances to a JSON file & deserializes back to instances"""
 
     __file_path = "file.json"
     __objects = {}
 
+    # Updated dictionary with User class
+    __classes = {
+        "BaseModel": BaseModel,
+        "User": User
+    }
+
     def all(self):
-        """Returns the dictionary __objects."""
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
@@ -23,23 +27,19 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
-        obj_dict = {key: obj.to_dict()
-                    for key, obj in self.__objects.items()}
+        """Serializes __objects to the JSON file"""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects if it exists."""
+        """Deserializes the JSON file to __objects (if it exists)"""
         try:
             with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
             for key, value in obj_dict.items():
-                # Extract the class name from the `__class__` key
-                class_name = value.pop("__class__", None)
-                if class_name:
-                    cls = globals().get(class_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
+                class_name = value["__class__"]
+                if class_name in self.__classes:
+                    self.__objects[key] = self.__classes[class_name](**value)
         except FileNotFoundError:
             pass
