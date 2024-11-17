37a3e292e105bda05f05f164e97e3ebb65ae9288
FRANKD-UX <frankdafrica@gmail.com>
2024-11-17 11:13:52 -0800

    F

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 # models/__init__.py
 
+# Ensure imports are at the top of the file
+
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
+# Dictionary to hold all model classes
+all_classes = {
+            'BaseModel': BaseModel,
+            }
+
+# Create an instance of FileStorage and reload
 storage = FileStorage()
 storage.reload()
