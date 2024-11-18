9ac77bbc64e5bf43f590b077d2ff4967166de26d
FRANKD-UX <frankdafrica@gmail.com>
2024-11-17 16:00:42 -0800

    F

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""FileStorage class for storing and retrieving objects"""
 import json
 from models.base_model import BaseModel
-from models.user import User  # Import User class
+from models.user import User
 
 
 class FileStorage:
-    """Serializes instances to a JSON file & deserializes back to instances"""
+    """Serializes instances to a JSON file and deserializes them back"""
 
     __file_path = "file.json"
     __objects = {}
 
-    # Updated dictionary with User class
-    __classes = {
-        "BaseModel": BaseModel,
-        "User": User
-    }
+    __classes = {"BaseModel": BaseModel, "User": User}
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -28,18 +23,21 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+            json.dump(
+                {key: obj.to_dict()
+                 for key, obj in self.__objects.items()}, f
+            )
 
     def reload(self):
-        """Deserializes the JSON file to __objects (if it exists)"""
+        """Deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, "r") as f:
-                obj_dict = json.load(f)
-            for key, value in obj_dict.items():
-                class_name = value["__class__"]
-                if class_name in self.__classes:
-                    self.__objects[key] = self.__classes[class_name](**value)
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name = value.pop("__class__", None)
+                    if class_name in self.__classes:
+                        self.__objects[key] = self.__classes[class_name](
+                            **value)
         except FileNotFoundError:
             pass
